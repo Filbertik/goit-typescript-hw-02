0acab2da343f48b7239cb8af2ac8d62c02e7bd7f
filbertik <aton2019zvit@gmail.com>
2025-05-14 16:33:14 +0300

    fix error_msg and img_gallery

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,10 +1,24 @@
+// Edit and typefix
+import React from "react";
+// Edit and typefix
+
 import s from "./ErrorMessage.module.css";
 
-const ErrorMessage = ({ code, message }) => {
-    return <div className={s.error}>
-        <h2>{code}</h2>
-        <p>{message}</p>
-    </div>
+// const ErrorMessage = ({ code, message }) => {
+//     return <div className={s.error}>
+//         <h2>{code}</h2>
+//         <p>{message}</p>
+//     </div>
 
+// }
+
+// Edit and typefix
+interface ErrorMessageProps {
+  message: string;
 }
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
+  return <p className={s.error}>{message}</p>;
+};
+// Edit and typefix
+
 export default ErrorMessage;
--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,15 +1,37 @@
-import s from "./ImageGallery.module.css";
-// import ImageCard from "../ImageCard/ImageCard";
+// Edit and typefix
+import React from "react";
+import { ImageData } from "../../types/Image";
+// Edit and typefix
 
-const ImageGallery = ({ data, handleModal }) => {
+// import ImageCard from "../ImageCard/ImageCard";
+import s from "./ImageGallery.module.css";
+
+// const ImageGallery = ({ data, handleModal }) => {
+//   return (
+//     <ul className={s.gallery}>
+//       {data.map((image) => (
+//         <li className={s.card} key={image.id}>
+//           <ImageCard image={image} handleModal={handleModal} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// Edit and typefix
+interface ImageGalleryProps {
+  images: ImageData[];
+  handleModal: (image: ImageData) => void;
+}
+const ImageGallery: React.FC<ImageGalleryProps> = ({ images, handleModal }) => {
   return (
     <ul className={s.gallery}>
-      {data.map((image) => (
-        <li className={s.card} key={image.id}>
-          <ImageCard image={image} handleModal={handleModal} />
-        </li>
+      {images.map((image) => (
+        <ImageCard key={image.id} image={image} handleModal={handleModal} />
       ))}
     </ul>
   );
 };
+// Edit and typefix
+
 export default ImageGallery;
